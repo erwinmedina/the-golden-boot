2b207cd3666168b2bfc0e62128da688d5e18b154
Erwin Medina <erwin.l.medina@gmail.com>
2022-07-15 10:31:12 -0700

    adjustment to expandCard

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar({setId, setFilter}) {
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="#">The Golden Boot</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
             
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
