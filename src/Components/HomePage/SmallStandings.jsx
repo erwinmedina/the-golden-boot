acd97d7fb3c851f89381118adce018c4daee04f0
Erwin <erwin.l.medina@gmail.com>
2024-10-01 21:33:32 -0700

    Revised a css error in teams page

--- src/Components/HomePage/SmallStandings.jsx
+++ src/Components/HomePage/SmallStandings.jsx
@@ -1,7 +1,9 @@
 import "./SmallStandings.css"
+import { useState } from "react";
 
 export default function SmallStandings({ selectedTeams }) {
-    
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 380);
+
     function splitForms(string) {
         const newArray = string.split(",");
         const formsArray = [];
@@ -19,37 +21,64 @@ export default function SmallStandings({ selectedTeams }) {
         return formsArray
     }
 
+    const handleResize = () => {
+        setIsSmallScreen(window.innerWidth < 600);
+    };
+    window.addEventListener('resize', handleResize);
+    
     return (
         <div className="teamsPageSmallStandings">
             <table id="smallStandings" className="table table-striped table-dark">
                 <thead>
-                    <tr>
-                        <th className="overallText">#</th>
-                        <th colSpan={2}></th>
-                        <th className="overallText">W</th>
-                        <th className="overallText">D</th>
-                        <th className="overallText">L</th>
-                        <th className="overallText">Goals</th>
-                        <th className="overallText">GD</th>
-                        <th className="overallText">Points</th>
-                        <th className="overallText">Form</th>
-                    </tr>
+                    { isSmallScreen ?
+                        <tr>
+                            <th className="overallText">#</th>
+                            <th colSpan={2}></th>
+                            <th className="overallText">Goals</th>
+                            <th className="overallText">GD</th>
+                            <th className="overallText">P</th>
+                            <th className="overallText">Form</th>
+                        </tr>
+                        :
+                        <tr>
+                            <th className="overallText">#</th>
+                            <th colSpan={2}></th>
+                            <th className="overallText">W</th>
+                            <th className="overallText">D</th>
+                            <th className="overallText">L</th>
+                            <th className="overallText">Goals</th>
+                            <th className="overallText">GD</th>
+                            <th className="overallText">P</th>
+                            <th className="overallText">Form</th>
+                        </tr>
+                    }
                 </thead>
                 <tbody>
                     { selectedTeams?.length > 0 && selectedTeams.map((team, index) => 
-                        <tr>
-                            <td className="overallText">{team.position}</td>
-                            <td className=" teamImg"><img alt="stuff" src={team.team.crest}/></td>
-                            <td className="teamNames">{team.team.shortName}</td>
-                            <td className="overallText">{team.won}</td>
-                            <td className="overallText">{team.draw}</td>
-                            <td className="overallText">{team.lost}</td>
-                            <td className="overallText">{team.goalsFor}:{team.goalsAgainst}</td>
-                            <td className="overallText">{team.goalDifference}</td>
-                            <td className="overallText">{team.points}</td>
-                            <td className="overallText">{ splitForms(team.form)}</td>
-                        </tr>
-                    
+                        (isSmallScreen ? 
+                            <tr>
+                                <td className="overallText">{team.position}</td>
+                                <td className="teamNames teamImg"><img alt="stuff" src={team.team.crest}/></td>
+                                <td className="teamNames">{team.team.shortName}</td>
+                                <td className="overallText">{team.goalsFor}:{team.goalsAgainst}</td>
+                                <td className="overallText">{team.goalDifference}</td>
+                                <td className="overallText">{team.points}</td>
+                                <td className="overallText">{ splitForms(team.form)}</td>
+                            </tr>
+                            :
+                            <tr>
+                                <td className="overallText">{team.position}</td>
+                                <td className="teamNames teamImg"><img alt="stuff" src={team.team.crest}/></td>
+                                <td className="teamNames">{team.team.shortName}</td>
+                                <td className="overallText">{team.won}</td>
+                                <td className="overallText">{team.draw}</td>
+                                <td className="overallText">{team.lost}</td>
+                                <td className="overallText">{team.goalsFor}:{team.goalsAgainst}</td>
+                                <td className="overallText">{team.goalDifference}</td>
+                                <td className="overallText">{team.points}</td>
+                                <td className="overallText">{ splitForms(team.form)}</td>
+                            </tr>
+                        )
                     )}
                 </tbody>
             </table>
